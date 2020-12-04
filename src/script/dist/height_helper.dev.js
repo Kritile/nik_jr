109cc8068341aea78595f6cc5c5515399ab45f6e
Kritile <rusakov@ittest-team.ru>
2020-12-04 04:15:26 +0300

    Fix margins

--- src/script/dist/height_helper.dev.js
+++ src/script/dist/height_helper.dev.js
@@ -1,6 +1,15 @@
 "use strict";
 
-document.querySelector('body').style.height = window.innerHeight + "px";
-window.addEventListener('resize', function () {
+if (window.innerHeight < 800) {
+  document.querySelector('body').style.height = '820px';
+} else {
   document.querySelector('body').style.height = window.innerHeight + "px";
+}
+
+window.addEventListener('resize', function () {
+  if (window.innerHeight < 800) {
+    document.querySelector('body').style.height = '820px';
+  } else {
+    document.querySelector('body').style.height = window.innerHeight + "px";
+  }
 });
--- src/script/height_helper.js
+++ src/script/height_helper.js
@@ -1,4 +1,13 @@
-document.querySelector('body').style.height = window.innerHeight+"px";
-window.addEventListener('resize',()=>{
+if(window.innerHeight<800){
+    document.querySelector('body').style.height = '820px';
+}else{
     document.querySelector('body').style.height = window.innerHeight+"px";
+}
+window.addEventListener('resize',()=>{
+    if(window.innerHeight<800){
+        document.querySelector('body').style.height = '820px';
+    }else{
+        document.querySelector('body').style.height = window.innerHeight+"px";
+    }
+    
 })
